1ccfe3bc7beffdd34c31c2b3896c9665f48a052d
jose caro <josecaro0187@gmail.com>
2020-01-26 09:25:43 -0500

    funcion lambda, filter

--- funcionesLambda.py
+++ funcionesLambda.py
@@ -1,7 +1,7 @@
 numpar = lambda numero: numero%2 == 0
 print(numpar(2))
 
-#funcion filter
+#funcion filter se puede aplicar tanto a listas como a objetos
 lisnumeros =[2,3,5,6,7,8,9,10]
 print(list(filter(lambda num: num % 2 == 0,lisnumeros)))
 
@@ -18,10 +18,11 @@ class Empleado:
 empleados = [
     Empleado('juan',2000),
     Empleado('Maria',3000),
-    Empleado('Carlos',2500)
+    Empleado('Carlos',2500),
+    Empleado('Estjer',3500)
 ]
 
-resultado = filter(lambda empleado : empleado.salario <=2000,empleados)
+resultado = filter(lambda empleado : empleado.salario <= 2000,empleados)
 
 for res in resultado:
     print(res)
